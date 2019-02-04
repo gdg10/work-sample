4fbfe52a1db2b29c6ac8cbf5f480da106c4d63d6
Garrett <gdgrube@gmail.com>
2019-02-04 07:57:23 -0500

    Update csv_normalizer.py

--- csv_normalizer.py
+++ csv_normalizer.py
@@ -9,19 +9,19 @@ def main(f):
 	with io.open(f, mode='r', encoding='utf8', errors="replace") as csv_file:		#REQ_0.1 | read a CSV file on 'stdin'								
 		with open("OUTPUT_"+f, mode="w", encoding='utf-8') as norm_csv_file:		#REQ_0.2 | emit a normalize CSV file on 'stdout'
 			
-			csv_reader = csv.reader(csv_file, delimiter=",", quotechar='"') 		#REQ_5.2 | quotechar parameter handles commas in the address field
+			csv_reader = csv.reader(csv_file, delimiter=",", quotechar='"') 	#REQ_5.2 | quotechar parameter handles commas in the address field
 			csv_writer = csv.writer(norm_csv_file, delimiter=",",
 				quotechar='"', quoting=csv.QUOTE_MINIMAL)
 			
 			lineCount = 1
 			for row in csv_reader:
 				try:
-					if lineCount != 1:												#do not normalize header row
+					if lineCount != 1:								#do not normalize header row
 						row = nrmlz.normalize(row)
-				except:																#REQ_9 | print a warning to stderr if replacement makes data invalid						
+				except:											#REQ_9 | print a warning to stderr if replacement makes data invalid						
 					warnings.warn("error - row could not be parsed and was dropped from output") 
 				else:
-					csv_writer.writerow(row)										#REQ_9 | drop row from output if replacement makes data invalid										
+					csv_writer.writerow(row)							#REQ_9 | drop row from output if replacement makes data invalid										
 					lineCount += 1
 
 if(len(sys.argv) != 2):
