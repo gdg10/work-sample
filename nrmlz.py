d08263431389a6e8f412f7095c069ee0b2b12a49
Garrett <gdgrube@gmail.com>
2019-02-04 07:58:06 -0500

    Update nrmlz.py

--- nrmlz.py
+++ nrmlz.py
@@ -4,7 +4,7 @@
 import pytz, copy
 from datetime import *
 
-def normalize(r):						#divide and conquer the row! 			
+def normalize(r):					#divide and conquer the row! 			
 	nTime = normTime(r[0]);				#call the appropriate normalizing function for each entry type
 	nAddress = normAddress(r[1]);
 	nZIP = normZIP(r[2]);
@@ -19,9 +19,9 @@ def normTime(time):
 	nTime = copy.deepcopy(time)
 	try:
 		dt = datetime.strptime(nTime, "%d/%m/%y %I:%M:%S %p")				#parse time into datetime object
-		dt = pytz.timezone('US/Pacific').localize(dt)						#localize it to Pacific time
-		dt = dt.astimezone(pytz.timezone('US/Eastern'))						#REQ_2 | convert time stamp to Eastern time				
-		nTime = dt.isoformat()												#REQ_1 | timestamp column should be formatted in ISO-8601 format										
+		dt = pytz.timezone('US/Pacific').localize(dt)					#localize it to Pacific time
+		dt = dt.astimezone(pytz.timezone('US/Eastern'))					#REQ_2 | convert time stamp to Eastern time				
+		nTime = dt.isoformat()								#REQ_1 | timestamp column should be formatted in ISO-8601 format										
 	except:
 		return time
 	else:
@@ -34,13 +34,13 @@ def normAddress(address):
 def normZIP(ZIP):
 	zeros = 5 - len(ZIP)
 	while zeros != 0:
-		ZIP = '0' + str(ZIP)		#REQ_3 | ZIPs should be 5 digits - add leading zeros if needed 
+		ZIP = '0' + str(ZIP)	#REQ_3 | ZIPs should be 5 digits - add leading zeros if needed 
 		zeros-=1
 	#print(ZIP)
 	return str(ZIP)
 
 def normName(name):
-	nName = name.upper() 			#REQ_4 | convert names to all uppercase
+	nName = name.upper() 		#REQ_4 | convert names to all uppercase
 	return nName
 
 def normFooDur(fooDur):
@@ -58,7 +58,7 @@ def normTotDur(nFooDur, nBarDur):
 	return nTotDur
 
 def normNotes(notes):
-	nNotes = notes 						#REQ_8 | do not perform any transformations on notes
+	nNotes = notes 				#REQ_8 | do not perform any transformations on notes
 	return nNotes
 
 def sliceTime(time):				#helper function - slice out the individual components of the HH:MM:SS.MS time string
@@ -70,10 +70,10 @@ def sliceTime(time):				#helper function - slice out the individual components o
 	slicedTime = [hrs, mins, secs, msec]
 	return slicedTime
 
-def toFloatSeconds(slTime):						#helper function to normDur functions		
-	totalSeconds = 0;							#running total of seconds
+def toFloatSeconds(slTime):				#helper function to normDur functions		
+	totalSeconds = 0;				#running total of seconds
 	totalSeconds+= float(slTime[0])*60*60 		#convert hours to seconds and add to total
-	totalSeconds+= float(slTime[1])*60			#convert mins to seconds and add to total
-	totalSeconds+= float(slTime[2])				#add seconds to total
+	totalSeconds+= float(slTime[1])*60		#convert mins to seconds and add to total
+	totalSeconds+= float(slTime[2])			#add seconds to total
 	totalSeconds+= float(float(slTime[3])/1000)	#convert ms to seconds and add to total
-	return float(totalSeconds)					#return total as a float value
+	return float(totalSeconds)			#return total as a float value
